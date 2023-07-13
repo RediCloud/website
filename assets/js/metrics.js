60a4d71804f364b431de5be26070b082b6397ec0
Suqatri <paul@suqatri.net>
2023-07-13 21:29:18 +0200

    impl cloud metrics

---
+++ assets/js/metrics.js
@@ -0,0 +1,28 @@
+const metricsUrl = "https://api.redicloud.dev/metrics/CloudV2_Build/"
+
+function setConnectedNodes(value) {
+    for (let elementsByClassNameElement of document.getElementsByClassName("metrics-connected-nodes")) {
+        elementsByClassNameElement.innerHTML = value;
+    }
+}
+function setRegisteredNodes(value) {
+    for (let elementsByClassNameElement of document.getElementsByClassName("metrics-registered-nodes")) {
+        elementsByClassNameElement.innerHTML = value;
+    }
+}
+
+function contentOfUrl(url) {
+    let hxr = new XMLHttpRequest();
+    hxr.open("GET", url);
+    hxr.send();
+    return hxr;
+}
+
+function updateMetrics() {
+    const connected = contentOfUrl(metricsUrl + "nodes/online")
+    connected.onload = () => setConnectedNodes(connected.responseText);
+    const registered = contentOfUrl(metricsUrl + "nodes/registered")
+    registered.onload = () => setRegisteredNodes(registered.responseText);
+    setTimeout(updateMetrics, 5000);
+}
+updateMetrics();
